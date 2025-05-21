89d822d772dd5b2b5e207011ca9414084315fd6b
Thanawat Panpinij <thanawatpanpinij@gmail.com>
2025-05-22 01:06:49 +0700

    feat: add frontend url to solve api usage issue

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(helmet());
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://notura-app-frontend.vercel.app/"],
   credentials: true,
 };
 app.use(cors(corsOptions));

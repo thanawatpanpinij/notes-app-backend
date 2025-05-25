ae0c047d8a6ebd1153cf9f66e761908da0a42200
Thanawat Panpinij <thanawatpanpinij@gmail.com>
2025-05-25 15:53:45 +0700

    feat(note): remove tags field from note model

--- src/models/Note.model.js
+++ src/models/Note.model.js
@@ -3,7 +3,6 @@ import { model, Schema } from "mongoose";
 const NoteSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  tags: { type: [String], default: [] },
   isPinned: { type: Boolean, default: false },
   userId: { type: String, required: true },
   createdOn: { type: Date, default: new Date().getTime() },
